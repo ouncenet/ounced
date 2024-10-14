08a26781b16c621ec54a133c2a5b9d2b7a40726e
heavyounces <heavyounces@proton.me>
2024-10-14 18:56:13 +0100

    A

--- cmd/kaspawallet/start_daemon.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/ouncenet/ounced/cmd/ouncewallet/daemon/server"
-
-func startDaemon(conf *startDaemonConfig) error {
-	return server.Start(conf.NetParams(), conf.Listen, conf.RPCServer, conf.KeysFile, conf.Profile, conf.Timeout)
-}
